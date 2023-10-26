7da0315e1dda2da283d4546ee3d37832081c771e
Andrei Li <andreiliphd@gmil.com>
2023-10-26 18:21:47 +0300

    OUOI

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -1,69 +1,46 @@
-import axios, {isCancel} from "../../node_modules/axios/dist/node/axios.cjs";
-import type { Config, Context } from "@netlify/edge-functions";
-
-
-export default async (req, context) => {
-    console.log(req.body);
+export default async (request, context) => {
+    let req = await request.json();
     let data = {};
-    data['location'] = req.body.location;
-    data['date'] = req.body.date;
+    data['location'] = req.location;
+    data['date'] = req.date;
     data['error'] = '';
-    let res = new Response();
+    console.log(req);
+    let answer = {};
+    
+    // Example POST method implementation:
+    async function getData(url = "") {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+    }
+   
     // Getting lat and lon from Geoname API
-    axios.get('http://api.geonames.org/searchJSON?q=' + req.body.location + '&maxRows=1&username=andreiliphd')
-        .then(function (response) {
-            
-        // handle success
-        console.log(response.data);
-        data['lng'] = response.data.geonames[0].lng;
-        data['lat'] = response.data.geonames[0].lat;
-        ;
-        console.log(data);
-    })
-    .catch(function (error) {
-    // handle error
-        console.log(error);
-        data['error'] = 'Error has happened during making request to external API';
-        res.body = data;
-    })
-    .then(function () {
-        // Getting information about weather from Weatherbit API using lat and lon
-        axios.get('http://api.weatherbit.io/v2.0/forecast/daily?'+ '&lat=' + data['lat'] + '&lon=' + data['lng'] + '&key=' + process.env.weatherbit)
-        .then(function (response) {
-            
-            // handle success
-            data['weather'] = response.data
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-            data['error'] = 'Error has happened during making request to external API';
-            res.body = data;
-        })
-        .then(function () {
-            // Getting picture city picture from Pixabay API
-            axios.get('https://api.unsplash.com/search/photos?query=' + data['location'].replace(/\s/g, '+') + '&client_id=y9qmhW-78MRf7O9A8AO1ShX-cbXTIdEaeDIXcjy-45k')
-            .then(function (response) {
-                // handle success
-                console.log(response.data);
-                data['img'] = response.data.results[2].urls.small;
-                console.log(data);
-            })
-            .catch(function (error) {
-            // handle error
-                console.log(error);
-                data['img'] = 'https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-600x490.jpg.webp';
-                data['error'] = 'Picture not found in Pixabay API';
-                res.body = data;
-            })
-            .then(function () {
-            // always executed
-                res.body = data;
-        });
-        });
-    });    
+    let lnglat = await getData('http://api.geonames.org/searchJSON?q=' + req.location + '&maxRows=1&username=andreiliphd');
+    console.log(lnglat);
+    data['lng'] = lnglat.geonames[0].lng;
+    data['lat'] = lnglat.geonames[0].lat;
+    let wbit = await getData('http://api.weatherbit.io/v2.0/forecast/daily?'+ '&lat=' + data['lat'] + '&lon=' + data['lng'] + '&key=' + "d4c9f9c4f54842ac8faa3085cfbb3e1e")
+    console.log(wbit);
+    data['weather'] = wbit.data;
+    let splg = await getData('https://api.unsplash.com/search/photos?query=' + data['location'] + '&client_id=y9qmhW-78MRf7O9A8AO1ShX-cbXTIdEaeDIXcjy-45k')
+    console.log(splg);
+    data['img'] = splg.results[2].urls.small; 
+    console.log(data);
+    console.log("fin");    
+    return new Response(JSON.stringify(data), {"status": 200});    
 };
 
-export const config: Config = {
+export const config = {
   path: "/weather",
 };
