0db056a45700bd80e0ffc33f50feb49a33d62d2b
Andrei Li <andreiliphd@gmail.com>
2023-10-27 15:45:35 +0300

    3466

--- netlify/functions/rain.mjs
+++ netlify/functions/rain.mjs
@@ -1,16 +1,16 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || 'GRY'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Intender usted Mr ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
     }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 350, body: error.toString() }
   }
 }
 
